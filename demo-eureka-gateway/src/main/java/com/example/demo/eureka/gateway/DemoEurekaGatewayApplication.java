c9f35304597dd3a6ec5d859f08d2ef2a56fcad2b
cuizq0907 <1312130479@qq.com>
2019-05-08 15:58:38 +0800

    gateway网关

---
+++ demo-eureka-gateway/src/main/java/com/example/demo/eureka/gateway/DemoEurekaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.eureka.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaGatewayApplication.class, args);
+    }
+
+}
