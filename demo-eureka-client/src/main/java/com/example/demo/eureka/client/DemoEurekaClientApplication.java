f9b6c87f8fbce7770b9ed79f1da629a25b108354
cuizq0907 <1312130479@qq.com>
2019-12-05 18:03:44 +0800

    jdk8便捷式开发，函数式接口、lambda、方法引用

--- demo-eureka-client/src/main/java/com/example/demo/eureka/client/DemoEurekaClientApplication.java
+++ demo-eureka-client/src/main/java/com/example/demo/eureka/client/DemoEurekaClientApplication.java
@@ -1,38 +1,15 @@
 package com.example.demo.eureka.client;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RefreshScope
-@RestController
 public class DemoEurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoEurekaClientApplication.class, args);
-    }
-
-    @Value("${server.port}")
-    String port;
-
-    @Value("${Hello}")
-    String hello;
-
-    @RequestMapping("/")
-    public String home() {
-        return "hello world from port: " + port;
-    }
-
-    @RequestMapping("/hello")
-    public String hello() {
-        return hello + "--8762";
     }
 
 }
